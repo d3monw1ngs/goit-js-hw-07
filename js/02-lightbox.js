a11d2073da23c69d697422149071f13cbb2884b6
d3monw1ngs <tezzielumsit87@gmail.com>
2024-02-09 14:26:44 +0800

    update js files

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+    let li = `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </li>
+    `;
+    gallery.innerHTML += li;
+});
+
+const galleryHandler = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250} );
+galleryHandler.on('show.simpleLightbox');
